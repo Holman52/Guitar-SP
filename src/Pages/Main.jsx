fd1d6d21289b9af97a099bb185f998f4b1ceb8ac
Ivan Holm <vinokur4515@gmail.com>
2025-11-18 01:23:01 +0300

    базовые стили для текста

---
+++ src/Pages/Main.jsx
@@ -0,0 +1,10 @@
+import Navigation from "../components/navigation/Navigation.jsx";
+
+const Main = () => {
+    return (
+        <div>
+            <Navigation />
+        </div>
+    )
+}
+export default Main
---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,30 @@
+const Navigation = () => {
+    return (
+        <header>
+            <nav>
+                <img src="#" alt="logo"/>
+                <div>
+                    <a href="#">Каталог</a>
+                    <a href="#">Хит продаж</a>
+                    <a href="">О нас</a>
+                    <a href="">Контакты</a>
+                </div>
+                <div>
+                    <a href="">
+                        <img src="f" alt="favorite"/>
+                        <p>Избранное</p>
+                    </a>
+                    <a href="">
+                        <img src="f" alt="favorite"/>
+                        <p>Корзина</p>
+                    </a>
+                    <a href="">
+                        <img src="f" alt="favorite"/>
+                        <p>Профиль</p>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
+}
+export default Navigation
