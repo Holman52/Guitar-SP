ff28428c778e3aeca331105e14558de38f5b8cc7
Ivan Holm <vinokur4515@gmail.com>
2025-11-19 02:07:12 +0300

    шаблон под карточки с преимущетсвом

--- src/components/GuitarService.jsx
+++ src/components/GuitarService.jsx
@@ -1,6 +1,62 @@
 import CardPublic from "./common/CardPublic.jsx";
 
 const GuitarService = () => {
+    const data = [
+        {
+            header: "Бесплатная пожизненная настройка каждой купленной гитары",
+            title: "ваше идеальное звучание начинается здесь и сейчас",
+            display: '',
+            count: 01
+        },
+        {
+            header: "Экспертная консультация от музыкантов",
+            title: "помогут подобрать инструмент менно под ваш стиль и уровень",
+            display: '',
+            count: 02
+        },
+        {
+            header: "Гарантия лучшей цены",
+            title: "возможность торговли-in вашего старого инструмента",
+            display: '',
+            count: 03
+        },
+        {
+            header: "Профессиональный подбор и setup гитары под вас",
+            title: "",
+            display: 'none',
+            count: <img alt='dddadsads' src='/public/guitar-service.png' />
+        },
+        {
+            header: "Выгодные цены и бонусы для постоянных клиентов",
+            title: "",
+            display: 'none',
+            count:  <img alt='dddadsads' src='/public/Coins.png' />
+        },
+        {
+            header: "Быстрая доставка по всей стране и удобный возврат",
+            title: "",
+            display: 'none',
+            count:  <img alt='dddadsads' src='/public/Delivery.png' />
+        },
+        {
+            header: "Только проверенные бренды и отборные экземпляры",
+            title: "мы сами тестируем каждую гитару перед продажей",
+            display: '',
+            count: 04
+        },
+        {
+            header: "Расширенная гарантия 3 года",
+            title: "сервисное обслуживание в подарок к любой покупке",
+            display: '',
+            count: 05
+        },
+        {
+            header: "Гибкие варианты рассрочки",
+            title: "без переплат, чтобы музыка была доступна каждому",
+            display: '',
+            count: 06
+        },
+    ];
     return (
         <CardPublic></CardPublic>
     )
--- src/components/common/CardPublic.jsx
+++ src/components/common/CardPublic.jsx
@@ -5,15 +5,14 @@ import '../../styles/base/common/CardPublic.scss'
 
 const GuitarService = (props) => {
     return (
-       <div>
+
+       <div className="service">
            <div className="guitar-service">
-               <div className="guitar-service__count">01</div>
-                <div className="guitar-service__text">
-                    <div className="guitar-service__text-header">Бесплатная пожизненная настройка
-                        каждойкупленной гитары</div>
-                    <div className={`guitar-service__text-header ${props.display}`}>ваше идеальное звучание начинается
-                        здесь и сейчас</div>
-                </div>
+               <div className="guitar-service__text">
+                   <h3 className="guitar-service__text-header">{props.header}</h3>
+                   <p className={`guitar-service__text-header ${props.display}`}>{props.title}</p>
+               </div>
+               <div className="guitar-service__count">{props.count}</div>
            </div>
        </div>
     );
