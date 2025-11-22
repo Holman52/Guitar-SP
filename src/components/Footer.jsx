1aa0910c6e77b5934237af0cff7be8697083ab8f
Ivan Holm <vinokur4515@gmail.com>
2025-11-22 05:39:42 +0300

    пробный запуск ссылок

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,9 +14,9 @@ const Footer = () => {
                 <nav className="footer-content">
                     <p className='footer-header'>Навигация</p>
                     <ul className='footer-text'>
-                        <li className='footer-text__desc'><a href="#/catalog">Каталог</a></li>
-                        <li className='footer-text__desc'><a href="#/about">О нас</a></li>
-                        <li className='footer-text__desc'><a href="#/contact">Контакты</a></li>
+                        <li className='footer-text__desc'>add</li>
+                        <li className='footer-text__desc'><router-link to="/about">О нас</router-link></li>
+                        <li className='footer-text__desc'><router-link to="/contact">Контакты</router-link></li>
                     </ul>
                 </nav>
                 <div className="footer-content">
